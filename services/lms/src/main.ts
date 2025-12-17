86b1c9ad617fa06e7dbab225338be9fbda1d94e4
anviiiijain <anvijain250102@gmail.com>
2025-12-17 22:03:49 +0530

    feat(lms-service): setup base lms service with schema

---
+++ services/lms/src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: { host: '0.0.0.0', port: 8001 },
+    },
+  );
+  
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  
+  await app.listen();
+  console.log('LMS Service on TCP 8001');
+}
+bootstrap();
